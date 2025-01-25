fc07a7597a199abd001f83b7162eb890c0f42374
veck01 <kasier48@gmail.com>
2025-01-25 16:16:39 +0900

    project.py 개발 완료료

--- src/project.py
+++ src/project.py
@@ -8,49 +8,58 @@ st.title("Fashion Recommendation Bot")
 openai_key = os.getenv("OPENAI_API_KEY")
 model = ChatOpenAI(model="gpt-4o", api_key=openai_key)
 
-if image := st.file_uploader("본인의 전신이 보이는 사진을 올려주세요!", type=['png', 'jpg', 'jpeg']):
-  st.image(image)
-  image = base64.b64encode(image.read()).decode("utf-8")
-  with st.chat_message("assistant"):
-    message = HumanMessage(
+if 'image_urls' not in st.session_state:
+  st.session_state.image_urls = []
+  
+if 'messages' not in st.session_state:
+  st.session_state.messages = []
+
+uploaded_files = st.file_uploader("본인의 전신이 보이는 사진을 올려주세요! (여러 장 업로드 가능)", type=['png', 'jpg', 'jpeg'], accept_multiple_files=True)
+
+def upload_image(files) -> str:
+  st.image([file for file in uploaded_files], caption=[f"업로드된 이미지 {i+1}" for i, file in enumerate(uploaded_files)], use_column_width=True)
+
+  for image in files:
+    image = base64.b64encode(image.read()).decode("utf-8")
+    image_url = f"data:image/jpeg;base64,{image}"
+    
+    if image_url not in st.session_state.image_urls:
+      st.session_state.image_urls.append(image_url)
+
+if uploaded_files:
+  upload_image(uploaded_files)
+  
+for m in st.session_state.messages:
+  with st.chat_message(m['role']):
+    st.markdown(m['content'])
+  
+if prompot := st.chat_input("질문을 입력하세요", key="user_input"):
+  with st.chat_message('user'):
+    st.markdown(prompot)
+  
+  with st.chat_message('assistant'):
+    messages = []
+    for m in st.session_state.messages:
+      role = m.get('role')
+      if role == 'user':
+        messages.append(HumanMessage(content=m['content']))
+      elif role == 'assistant':
+        messages.append(AIMessage(content=m['content']))
+    
+    cur_message = HumanMessage(
       content=[
-        {"type": "text", "text": "사람의 전신이 찍혀있는 사진이 한 장 주어집니다. 이 때, 사진 속 사람과 어울리는 옷 및 패션 스타일을 추천해주세요."},
-        {
-          "type": "image_url",
-          "image_url": {"url": f"data:image/jpeg;base64,{image}"},
-        },
-      ],
+        {"type": "text", "text": prompot}
+      ]
     )
-    result = model.invoke([message])
+    
+    for url in st.session_state.image_urls:
+      cur_message.content.append({"type": "image_url", "image_url": { "url" : url}})
+    
+    st.session_state.messages.append({"role": "user", "content": prompot})
+    
+    messages.append(cur_message)
+    result = model.invoke(messages)
     response = result.content
+    
     st.markdown(response)
-
-# if "messages" not in st.session_state:
-#   st.session_state.messages = []
-    
-# for message in st.session_state.messages:
-#   with st.chat_message(message["role"]):
-#     st.markdown(message["content"])
-    
-# if prompt := st.chat_input("What is up?"):
-#     with st.chat_message("user"):
-#         st.markdown(prompt)
-#     st.session_state.messages.append({"role": "user", "content": prompt})
-
-#     with st.chat_message("assistant"):
-#         messages = []
-#         for m in st.session_state.messages:
-#             if m["role"] == "user":
-#                 messages.append(HumanMessage(content=m["content"]))
-#             else:
-#                 messages.append(AIMessage(content=m["content"]))
-
-#         result = model.invoke(messages)
-#         response = result.content
-        
-#         st.markdown(response)
-
-#         st.session_state.messages.append({
-#             "role": "assistant",
-#             "content": response
-#         })
+    st.session_state.messages.append({"role": "assistant", "content": response})
